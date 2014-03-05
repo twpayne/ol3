34d2b394aa07ae46a55c1bd2f9197383728fb852
ahocevar <andreas.hocevar@gmail.com>
2014-03-05 18:00:54 +0100

    Note about MultiGeometry rendering

--- examples/modify-features.js
+++ examples/modify-features.js
@@ -166,7 +166,7 @@ var vectorLayer = new ol.layer.Vector({
 });
 
 // FIXME Handle this elsewhere - this is only to not render selected features
-// on the original layer
+// on the original layer, and it does not work for MultiGeometry geometries.
 vectorLayer.setRenderGeometryFunctions(new ol.Collection([function(geometry) {
   var selected = select.getFeatures().getArray();
   for (var i = 0, ii = selected.length; i < ii; ++i) {
